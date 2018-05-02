23fe73c178c2ca4b0f670d5cdab9cdf9236aad17
Asia <asialuk98@gmail.com>
2018-05-02 22:33:03 +0200

    Add constructors in IndexPair class

--- src/data/IndexPair.java
+++ src/data/IndexPair.java
@@ -1,9 +1,24 @@
 package data;
 
+import data.CSVparser;
+
 public class IndexPair {
 	
 	private int firstIndex;
 	private int secondIndex;
+	
+	public IndexPair() {
+		CSVparser parser = new CSVparser("z3data11.csv");
+		firstIndex = parser.getFileData().get(0);
+		secondIndex = parser.getFileData().get(parser.getFileData().size() - 1);
+	}
+	
+	public IndexPair(int firstIndex, int secondIndex) {
+		this.firstIndex = firstIndex;
+		this.secondIndex = secondIndex;
+	}
+	
+	
 	
 	public int getFirstIndex() {
 		return firstIndex;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,29 @@
 package main;
 
 import data.CSVparser;
+import stack_implementation.MyStack;
+import stack_implementation.IStack;
+import data.IndexPair;
+import stack_implementation.EmptyStackException;
+
 
 public class Main {
-	public static void main (String [] args) {
+	public static void main (String [] args) throws EmptyStackException{
 		
 		CSVparser parser = new CSVparser("z3data11.csv");
 		parser.read();
 		
-		for(Integer number : parser.getFileData()) {
-			System.out.print(number + " ");
-		}
+//		for(Integer number : parser.getFileData()) {
+//			System.out.print(number + " ");
+//		}
+//		
+		
+		MyStack stack = new MyStack<IndexPair>();
+		
+		IndexPair ip = new IndexPair(1, 4);
+		stack.push(ip);
+		System.out.println(((IndexPair) stack.pop()).getFirstIndex());
+
+		
 	}
 }
