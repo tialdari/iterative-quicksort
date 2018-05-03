cecd06433d9c4ed39eba1b01558aaee2ad6ae533
Asia <asialuk98@gmail.com>
2018-05-03 15:19:59 +0200

    Add pivot choice option

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,29 +11,37 @@ import java.util.ArrayList;
 public class Main {
 	public static void main (String [] args) throws EmptyStackException{
 		
-		CSVparser parser = new CSVparser("z3data11.csv");
+		CSVparser parser = new CSVparser("z3data33.csv");
 		parser.read();
 		
 	
 		QuickSort qs = new QuickSort();
 		
+		
 		ArrayList<Integer> demoList = new ArrayList<Integer>();
 		
-		demoList.add(1);
-		demoList.add(7);
-		demoList.add(3);
-		demoList.add(8);
-		demoList.add(0);
 		demoList.add(2);
-		demoList.add(4);
+		demoList.add(7);
+		demoList.add(10);
+		demoList.add(11);
+		demoList.add(24);
+		demoList.add(8);
+		demoList.add(10);
+		demoList.add(1);
+		demoList.add(0);
+		demoList.add(15);
+
 
 
 		qs.sorting(demoList);
+		
+	
+		
 		
 		for(Integer number : demoList) {
 			System.out.print(number + " ");
 		
 		}
-			
+		
 	}
 }
--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import stack_implementation.MyStack;
 import data.IndexPair;
 import stack_implementation.EmptyStackException;
+import java.util.Random;
 
 public class QuickSort {
 	
@@ -14,9 +15,41 @@ public class QuickSort {
 		toBeSorted.set(secondIndex, temporary);
 	}
 	
-	public int partition(ArrayList<Integer> toBeSorted, int start, int end) {
-
-		int pivot = toBeSorted.get(end);						// set the pivot as the last element
+	public int partition(ArrayList<Integer> toBeSorted, int start, int end, String pivotChoice) {
+		
+		int pivot;
+		int temporary;
+		
+		
+		if(pivotChoice == "last") {					//dla danego przedziału ArrayListy wyznaczamy pivota wg danego kryterium
+			
+			pivot = toBeSorted.get(end);				
+	 
+		}else if(pivotChoice == "random") {
+		
+			Random rdm = new Random();
+			int random = start - 1;
+			while(random < start) {
+				random = rdm.nextInt(end);
+			}
+			pivot = toBeSorted.get(random);
+			
+			temporary = toBeSorted.get(end);
+			toBeSorted.set(end, pivot);
+			toBeSorted.set(random, temporary);
+			
+		
+		}else if(pivotChoice == "median"){
+		
+			pivot = pivotMedian(toBeSorted, start, end);
+			
+			temporary = toBeSorted.get(end);
+			toBeSorted.set(end, pivot);
+			toBeSorted.set(toBeSorted.indexOf(pivot), temporary);
+		}else {
+			pivot = 0;
+		}
+		
 		
 		int pivotIndex = start;
 		
@@ -47,9 +80,9 @@ public class QuickSort {
 
 			stack.pop();
 			
-			int pivotIndex = partition(toBeSorted, start, end);
+			int pivotIndex = partition(toBeSorted, start, end, "median");
 			
-			System.out.println(start + " " + end + " "+ pivotIndex);
+			//System.out.println(start + " " + end + " "+ pivotIndex);
 
 			
 			if(start < pivotIndex - 1) {
@@ -60,6 +93,41 @@ public class QuickSort {
 				stack.push(new IndexPair(pivotIndex + 1, end));
 			}
 		}
+	}
+	
+	public int pivotMedian(ArrayList<Integer> toBeSorted, int lowIndex, int highIndex) {		//wylicza pivota będącego medianą liczb: 
+														//pierwszej, ostatniej i środkowej z danego przedziału ArrayListy	
+		int pivotMedian = 0;
+		int firstNumber = toBeSorted.get(lowIndex);
+		int median = toBeSorted.get((int) Math.floor((lowIndex + highIndex) / 2));
+		int lastNumber = toBeSorted.get(highIndex);
+		
+		double average = (firstNumber + lastNumber + median) / 3;					//średnia arytmetyczna wskazanych liczb
+		
+		int [] abs = new int[3];
+		abs[0] = Math.abs((int)average - firstNumber);		//obliczanie wartości bezwzględnej różnicy pomiędzy każdą z liczb, 
+		abs[1] = Math.abs((int)average - lastNumber);			//a średnią arytmetyczną
+		
+		abs[2] = Math.abs((int)average - median);
+		
+		
+		int min = abs[0];
+		
+		for(int k = 0; k < 3; k++) {				//liczba, dla wyliczona wartość bezwzględna jest najmniejsza, będzie szukanych pivotem
+			if(abs[k] < min) {					
+			
+			min = abs[k];
+			}
+		}
+		
+		if(min == abs[0]) {
+			pivotMedian = firstNumber;
+		}else if (min == abs[1]) {
+			pivotMedian = lastNumber;
+		}else if(min == abs[2]) {
+			pivotMedian = median;
+		}	
+		return pivotMedian;
 	}
 	
 }
